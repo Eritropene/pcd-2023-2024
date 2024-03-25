6d375beaf0b897066bbb2a0899fafa6f62cd8e24
Michele Ravaioli <mikirava01@gmail.com>
2024-03-25 13:10:03 +0100

    i dont know

--- app/src/main/java/pcd/lab03/liveness/accounts_exercise/AccountManager.java
+++ app/src/main/java/pcd/lab03/liveness/accounts_exercise/AccountManager.java
@@ -1,18 +1,41 @@
 package pcd.lab03.liveness.accounts_exercise;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class AccountManager {
 	
 	private final Account[] accounts;
+	private final Lock[] locks;
 
 	public AccountManager(int nAccounts, int amount){
 		accounts = new Account[nAccounts];
+		locks = new Lock[nAccounts];
 		for (int i = 0; i < accounts.length; i++){
 			accounts[i] = new Account(amount);
+			locks[i] = new ReentrantLock();
 		}
 	}
 	
 	public void transferMoney(int from,	int to, int amount) throws InsufficientBalanceException {
-		throw new RuntimeException("To be implemented");
+		final Account sender = this.accounts[from];
+		final Account receiver = accounts[to];
+		try {
+			locks[Math.min(from, to)].lockInterruptibly();
+			locks[Math.max(from, to)].lockInterruptibly();
+
+			if (sender.getBalance() < amount) {
+				throw new InsufficientBalanceException();
+			}
+			receiver.credit(amount);
+			sender.credit(amount);
+
+		} catch (InterruptedException ignored) {
+		} finally {
+			locks[Math.min(from, to)].unlock();
+			locks[Math.max(from, to)].unlock();
+		}
 	}
 	
 	public int getNumAccounts() {
--- app/src/main/java/pcd/lab04/sem/ex/Pinger.java
+++ app/src/main/java/pcd/lab04/sem/ex/Pinger.java
@@ -1,14 +1,22 @@
 package pcd.lab04.sem.ex;
 
+import java.util.concurrent.Semaphore;
+
 public class Pinger extends Thread {
 
-	public Pinger() {
+	private final Semaphore pingEvent, pongEvent;
+	public Pinger(Semaphore pingEvent, Semaphore pongEvent) {
+		this.pingEvent = pingEvent;
+		this.pongEvent = pongEvent;
 	}	
 	
 	public void run() {
 		while (true) {
 			try {
+				pongEvent.acquire();
 				System.out.println("ping!");
+				pingEvent.release();
+				sleep(1000);
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
